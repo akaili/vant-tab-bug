4b37d07944f64f574a34cfe7b6552b2a72b36601
Zac Li Qianlan <zac.li@qlcloud.ltd>
2022-06-22 17:36:21 +0800

    重现 tabs bug

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>最后一个选中时文字颜色相对其他比较浅</p>
+    <van-tabs v-model="active">
+      <van-tab v-for="item in tabs.filter((item, index) => index < 6)" :key="item.id" :title="item.name"></van-tab>
+    </van-tabs>
+
+    <p>正常情况1</p>
+    <van-tabs v-model="active">
+      <van-tab v-for="item in tabs" :key="item.id" :title="item.name"></van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import list from './data.json';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      active: '',
+      tabs: list,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="less">
+@Regular: 'SourceHanSansCN-Regular';
+@Medium: 'SourceHanSansCN-Medium';
+@Bold: 'SourceHanSansCN-Bold';
+
 #app {
+  height: 100vh;
+  background-color: #f2f2f2;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px 14px;
+}
+
+.van-tabs__wrap {
+  height: 30px;
+  overflow: hidden;
+  border-radius: 6px;
+}
+
+.van-tab__text {
+  color: #333;
+  font-size: 13px;
+  font-family: @Regular;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.van-tab--active {
+  .van-tab__text {
+    color: #f5759a;
+    font-weight: bold;
+    font-size: 14px;
+    -webkit-tap-highlight-color: transparent;
+  }
+}
+
+.van-tabs__line {
+  width: 30px;
+  height: 6px;
+  // margin-bottom: -3px;
+  background-color: #f5759a !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import Vant from 'vant';
 import App from './App.vue'
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
